8d4f4d563db68fdfe9978c8707c3fde0011a308a
Asel Bekpolot <aselbekpolot@gmail.com>
2022-04-20 15:03:31 +0600

    Add

---
+++ src/main/java/org/prepro/Main.java
@@ -0,0 +1,10 @@
+package org.prepro;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger=Logger.getLogger(Main.class);
+        System.out.println("Hello All!");
+    }
+}
